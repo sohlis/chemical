1a8c7d9f9388023a6a93735b64ce3459be05da79
Stefan Sohlstrom <stefan.sohlstrom@gmail.com>
2015-07-12 15:47:24 -0700

    added an array of my own colors

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
-// var base = "#F37F23"
 
+var items = [
+              ['#FAE4E9','#BBACC1'],
+              ['#909580','#545643'],
+            ];
 
-// $(document).ready(function() {
-//     var color = chroma(base).brighten(10).hex();
-//     var colorAgin = chroma(base).darken(15).hex();
-//     var colorAginAgin = chroma(base).darken(20).hex();
-//   $("#chroma-test").css("background-color", color);
-//     $("#chroma-test2").css("background-color", base);
-//   $("#chroma-test3").css("background-color", colorAgin);
-//     $("#chroma-test4").css("background-color", colorAginAgin);
-// });
+var rand = items[Math.floor(Math.random() * items.length)];
+
+var myColorOne = rand[0];
+var myColorTwo = rand[1];
 
 // The following selects to colors at random from Chroma
-var colorOne = chroma.random();
-var colorTwo = chroma.random();
+var robotColorOne = chroma.random();
+var robotColorTwo = chroma.random();
+// Mixes the two colors. the value is where on the spectrum it should pull the mix from
+var colorOne = chroma.interpolate(myColorOne, robotColorOne, 0.01, 'lab');
+var colorTwo = chroma.interpolate(myColorTwo, robotColorTwo, 0.5, 'lab')
 // Then makes a scale between the two (accessible by `scale(0-1)`)
-var scale = chroma.scale([colorTwo, colorOne]);
+var scale = chroma.scale([colorTwo, colorOne]).mode('lab');
 // Initial value of the start of the scale
 var scaleValue = 0;
 // Finds how many .container s there are in the document, and divides by that number
 var scaleIncrementer = 1 / $('.container').length;
 
 for (var i = 0; i < $('.container').length; i++) { //for every .container in document do:
-
   $( ".container:nth-of-type(" + (i+1) +")" ).css("background-color", scale(scaleValue).hex());  // using the nth of type to select each container, the Chromajs scale to pick colors
-
   scaleValue = scaleValue + scaleIncrementer; // add it for more scales!
 };
